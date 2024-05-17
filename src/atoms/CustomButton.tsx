7aa22412264ca79524972b2c619b7edbceb34093
samir <arrahmani.samir@gmail.com>
2024-05-17 23:54:04 +0200

    wip config router and create not Found Page

---
+++ src/atoms/CustomButton.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+import { Button, Theme, useTheme } from '@mui/material';
+import { FC, JSX, ReactNode } from 'react';
+
+interface CustomButtonProps {
+  label: string;
+  onClick: () => void;
+  startIcon?: ReactNode;
+  endIcon?: ReactNode;
+  variant?: 'text' | 'outlined' | 'contained';
+}
+
+const CustomButton: FC<CustomButtonProps> = ({
+  label,
+  onClick,
+  startIcon,
+  endIcon,
+  variant = 'contained',
+}: CustomButtonProps): JSX.Element => {
+  const theme = useTheme();
+
+  return (
+    <StyledButton
+      type="button"
+      theme={theme}
+      variant={variant}
+      onClick={onClick}
+      startIcon={startIcon}
+      endIcon={endIcon}
+    >
+      {label}
+    </StyledButton>
+  );
+};
+
+export default CustomButton;
+
+const StyledButton = styled(Button)(
+  ({ theme }: { theme: Theme }) => `
+  min-width: 170px;
+  font-weight: 600;
+  border-radius: 20px;
+  font-size: 16px;
+  transition: background-color 0.3s, color 0.3s;
+  &:hover {
+    box-shadow: none;
+  }
+  ${theme.breakpoints.down('sm')} {
+    font-size: 12px;
+    min-width: 150px;
+  }
+`,
+);
---
+++ src/utils/constant.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: '#FF7500',
+  secondary: '#4BC0A1',
+  black: '#2E2D2B',
+  white: '#FFFFFF',
+  gray: '#9DA3B4',
+  transparent: '#00000000',
+};
