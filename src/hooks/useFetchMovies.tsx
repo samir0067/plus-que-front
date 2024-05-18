a4f4a2f3de87c40501c728694c0ada28e5dfa572
samir <arrahmani.samir@gmail.com>
2024-05-19 01:39:41 +0200

    ajout de hook personnaliser pour les appell api

---
+++ src/hooks/useFetchMovies.tsx
@@ -0,0 +1,68 @@
+import axios, { AxiosError } from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { ApiError, MovieResponse } from '../utils/types.ts';
+
+/**
+ * Interface for useFetchMovies hook return parameters.
+ */
+interface UseFetchMoviesParams {
+  data: MovieResponse | null;
+  loading: boolean;
+  error: string | null;
+}
+
+/**
+ * Custom Hook to retrieve the movie list from the API.
+ * @returns {UseFetchMoviesParams} List of films, loading status and errors.
+ */
+const useFetchMovies = (): UseFetchMoviesParams => {
+  const [data, setData] = useState<MovieResponse | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchMovies = useCallback(async () => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    if (!apiKey) {
+      setError('API key is not available');
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const response = await axios.get<MovieResponse | ApiError>('https://api.themoviedb.org/3/movie/popular', {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+
+      if ('status_code' in response.data) {
+        setError(response.data.status_message);
+        return;
+      }
+
+      setData(response.data);
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError<ApiError>;
+        if (axiosError.response) {
+          setError(axiosError.response.data.status_message);
+        } else {
+          setError(axiosError.message);
+        }
+      } else {
+        setError('An unknown error occurred');
+      }
+      console.error('Error fetching movies =>', error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    void fetchMovies();
+  }, [fetchMovies]);
+
+  return { data, loading, error };
+};
+
+export default useFetchMovies;
---
+++ src/utils/types.ts
@@ -0,0 +1,94 @@
+
+/**
+ * Interface for API errors.
+ */
+export interface ApiError {
+  success: boolean;
+  status_code: number;
+  status_message: string;
+}
+
+
+export interface BelongsToCollection {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompany {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: BelongsToCollection | null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  origin_country: string[];
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Movie {
+  backdrop_path: string;
+  id: number;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  media_type: string;
+  adult: boolean;
+  title: string;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
