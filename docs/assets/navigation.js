297ad834f391784bb2e166a6eb2f51d1e441b93d
samir <arrahmani.samir@gmail.com>
2024-05-19 00:54:56 +0200

    add documentation elements

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAE4uuVipJrShRslIqLU51Sy1JzvDNL8tMdUktSczMKVbSUSpILMkAyubmp5TmpBbrY1Oll1GSmwNUmp2Zl6JkZaSjlJyRmZNSlJqnZBUNNz4lNS2xNKcEYWJaaV5ySWZ+Hg4zoepRzTYzqY2t1cHuZEKOpa4z8TkwFgAI1nEaVQEAAA=="
